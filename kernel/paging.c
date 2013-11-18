1f04d758fbc748642df54e650f5b23b056376832
Peter Enns <peter.n.enns@gmail.com>
2013-11-17 23:12:27 -0500

    Emergency push before this computer runs out of batteries

--- kernel/paging.c
+++ kernel/paging.c
@@ -2,8 +2,10 @@
 #include <stddef.h>
 #include <printk.h>
 #include <string.h>
+
 #define MEM_SZ (1024 * 1024 * 4)
 #define PAGE_SZ (1024 * 4)
+#define ALIGN(addr) ((addr & ~0xFFF) + 0x1000)
 
 extern uint32_t end;
 uint32_t placement_addr = (uint32_t)&end;
@@ -22,6 +24,23 @@ void *kmalloc(size_t sz, uint32_t *ptr)
 	tmp = placement_addr;
 	placement_addr += sz;
 	return (void *) tmp;
+}
+
+uint32_t *frames;
+uint32_t top = 0;
+uint32_t nframes = 0;
+
+uint32_t pop_frame()
+{
+	if (top == nframes) {
+		// panic
+	}
+	return frames[top++];
+}
+
+void push_frame(uint32_t addr)
+{
+	frames[top--] = addr;
 }
 
 void switch_to_page_dir(struct page_directory *dir)
@@ -43,9 +62,12 @@ void paging_install()
 	memset(kernel_dir, 0, 1024 * sizeof(*kernel_dir));
 	table = kmalloc(sizeof(*table), NULL);
 	memset(table, 0, 1024 * sizeof(*table));
+	nframes = 0x1000000 / 0x1000; // Assuming memory is 16 MB for now
+	top = nframes - 1;
+	frames = kmalloc(nframes * sizeof(*frames), NULL);
 	
 	uint32_t i = 0;
-	for (i = 0; i < 1024; i++) {
+	for (i = 0; i < placement_addr; i++) {
 		table->pages[i].present = 1;
 		table->pages[i].writeable = 1;
 		table->pages[i].user = 0;
@@ -55,9 +77,9 @@ void paging_install()
 	kernel_dir->tables[0] = table;
 	kernel_dir->tables_physical[0] = ((uint32_t) table->pages) | 3;
 
-	struct page *tbl = (struct page *) (kernel_dir->tables_physical[0] & ~0xFFF);
-	for (i = 0; i < 1024; i++) {
-		printk("%x\n", tbl[i]);
+	for (i = ALIGN(placement_addr); i < 0x1000000; i += 0x1000) {
+		printk("%x\n", i);
+		push_frame(i);
 	}
 
 	switch_to_page_dir(kernel_dir);
